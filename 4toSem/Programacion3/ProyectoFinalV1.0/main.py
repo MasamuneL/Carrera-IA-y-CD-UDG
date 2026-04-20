b9a0df87371bd8721114dc20f7fdaf7ca8dc8e13
Vikingo <alanjosue.dev@gmail.com>
2026-04-20 12:00:03 -0600

    Cambios al main y modelo entrenado

--- 4toSem/Programacion3/ProyectoFinalV1.0/main.py
+++ 4toSem/Programacion3/ProyectoFinalV1.0/main.py
@@ -1,11 +1,31 @@
 import modelo
+import os
+from tensorflow import keras
 # import graficas
 
+RUTA_MODELO = "modelo_entrenado.keras"
+
+def guardar_modelo(modelo_entrenado, ruta=RUTA_MODELO):
+    modelo_entrenado.save(ruta)
+    print(f'✅ Modelo guardado en: {os.path.abspath(ruta)}')
+
+def cargar_modelo(ruta=RUTA_MODELO):
+    if os.path.exists(ruta):
+        modelo_cargado = modelo.load_model(ruta)
+        print(f'✅ Modelo cargado desde: {os.path.abspath(ruta)}')
+        return modelo_cargado
+    else:
+        print(f'❌ No se encontró el modelo en: {os.path.abspath(ruta)}')
+        return None
+    
 def prediccion(modelo):
     resutado = modelo.predict([100.0])
     print(f'la temperatura {100.0}°C es igual a {resutado}°F')
 
 if __name__ == '__main__':
-    modelo, historial = modelo.entrenar_modelo()
-    # graficas.graificas(historial)
-    prediccion(historial)
+    modelo_entrenado = cargar_modelo()
+
+    if modelo_entrenado is None:
+        print("No hay modelo entenado. Entrenando...")
+        modelo_entrenado, historial = modelo.entrenar_modelo()
+        guardar_modelo(modelo_entrenado)
